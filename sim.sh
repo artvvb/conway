7909c3ffc66b7720d4188a65387db3c34653bdee
ArtVVB <arthur.brown@digilent.com>
2025-03-28 17:08:41 -0700

    Remove bash build script - make does this now

--- sim.sh
+++
@@ -1,5 +0,0 @@
-xvlog --sv hdl/counter.sv
-xvlog --sv sim/counter_tb.sv
-xelab --debug typical --top counter_tb --snapshot counter_tb_snapshot
-xsim counter_tb_snapshot --tclbatch xsim_cfg.tcl
-xsim --gui ./counter_tb_snapshot.wdb
